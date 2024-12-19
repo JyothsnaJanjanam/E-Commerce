2c50dc8e253f2669bb2fabdcca1644e23ab80e04
JyothsnaJanjanam <janjanamjyothsna@gmail.com>
2024-12-19 20:22:07 +0530

    add frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,43 @@
+import './App.css';
+import { Navbar } from './Components/Navbar/Navbar';
+import { BrowserRouter, Router, Route, Routes } from 'react-router-dom'
+import ShopCategory from './Pages/ShopCategory';
+import Shop from './Pages/Shop';
+import LoginSignup from './Pages/LoginSignup';
+import Product from './Pages/Product';
+import Cart from './Pages/Cart';
+import Footer from './Components/Footer/Footer';
+import men_banner from './Components/Assets/banner_mens.png'
+import women_banner from './Components/Assets/banner_women.png'
+import kids_banner from './Components/Assets/banner_kids.png'
+
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+        <Navbar />
+        
+        <Routes>
+          <Route path='/' element={<Shop/>} />
+          <Route path='/men' element={<ShopCategory category='men' banner={men_banner} />} />
+          <Route path='/women' element={<ShopCategory category='women' banner={women_banner} />} />
+          <Route path='/kids' element={<ShopCategory category='kids' banner={kids_banner} />} />
+          <Route path='/product' element={<Product />}>
+            <Route path=':productId' element={<Product />} />
+          </Route>
+
+          <Route path='/cart' element={<Cart />} />
+          <Route path='/login' element={<LoginSignup />} />
+
+        </Routes>
+
+        <Footer />
+
+      </BrowserRouter>
+      
+
+    </div>
+  );
+}
+
+export default App;
